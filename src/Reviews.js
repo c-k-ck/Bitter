6062816e41dd2dda0215b904e3a2f4ccc7375a2e
Chyniece Matthews <cfmatthews2@gmail.com>
2023-06-14 22:25:43 -0500

    last udpate

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
 import { useAuth0 } from '@auth0/auth0-react';
 import AddReview from './AddReview';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +15,10 @@ export default function Reviews() {
     const [show, setShow] = useState(false);// control the modal
     const { getAccessTokenSilently } = useAuth0();
     const { isAuthenticated } = useAuth0();
-    
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [filteredReviews, setFilteredReviews] = useState([]);
+
     useEffect(() => {
         fetchReviews(); //runs the func when first rendered
     }, []);
@@ -102,6 +105,30 @@ export default function Reviews() {
         setEditingReview(review);
         setShow(true)
     };
+    const applyFilters = () => {
+        let filteredResults = reviews; // Start with all reviews
+
+        // Apply search query filter
+        if (searchQuery) {
+            filteredResults = filteredResults.filter((review) =>
+                review.title.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        }
+
+        // Apply category filter
+        if (selectedCategory) {
+            filteredResults = filteredResults.filter(
+                (review) => review.category === selectedCategory
+            );
+        }
+        console.log(filteredResults)
+        console.log(reviews)
+        setFilteredReviews(filteredResults); // Update filteredReviews
+    };
+
+    const handleSearch = () => {
+        applyFilters();
+    };
     const handleShow = () => setShow(true); // sets show to true which triggers modal for adding review
 
     return (
@@ -116,10 +143,29 @@ export default function Reviews() {
                 show={show}
                 onHide={() => setShow(false)}
             /></div>
+            <div className="search-form">
+
+                <Form.Select
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                >
+                    <option value="">All Categories</option>
+                    <option value="Restaurants & Food">#BiteSizedReviews 🍔</option>
+                    <option value="Movies & Entertainment">#CinemaCritic 🍿</option>
+                    <option value="Retail & Shopping">#RetailShowdown 🛍️</option>
+                    <option value="Travel & Hospitality">#TravelTrials ✈️</option>
+                    <option value="Education & Learning">#EducationEvaluations 🎓</option>
+                    <option value="Jobs & Workplaces">#TheOfficeChronicles 💼</option>
+                    <option value="Public Services & Government">#GovernmentGripes 🏛️</option>
+                    <option value="Healthcare Services">#HealthcareHeroes&Hurdles 🩺</option>
+                    <option value="Other">#TheUncharted 🎭</option>
+                </Form.Select>
+                <button className='LoginButton' style={{marginBottom: '10px', marginTop:'10px'}} onClick={handleSearch}>Search</button>
+            </div>
 
             <div className="cards">
-                {reviews.length > 0 ? ( //map iterates over each review and creates a card with buttons
-                    reviews.map((review) => (
+                {filteredReviews.length > 0 ? ( //map iterates over each review and creates a card with buttons
+                    filteredReviews.map((review) => (
                         <div key={review._id} >
                             <Card key={review._id}>
                                 <Card.Body>
